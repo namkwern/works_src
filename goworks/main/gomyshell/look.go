27ec5180dc891e9aad5e228fb9f392e986220e29
intelfike <intelfike@gmail.com>
2016-07-28 19:15:28 -0700

    最新

--- goworks/main/gomyshell/look.go
+++ goworks/main/gomyshell/look.go
@@ -13,23 +13,27 @@ import(
 var(
 	dFlag bool
 	namesub []string
-	textsub []string
 	namesubnot []string
-	textsubnot []string
-	inF bool
-	innF bool
+	linesub []string
+	linesubnot []string
+	diresub []string
+	diresubnot []string
+	lineF bool
 	nFlag bool
 	rFlag bool
 	count int
 )
 func main(){
+	
 	rF := flag.Bool("r", false, "!!!表示・処理過多注意!!!\n\tRecursive 下層のファイル/ディレクトリをすべて検索")
 	dF := flag.Bool("d", false, "Directory ディレクトリ検索に切り替え")
-	nF := flag.Bool("n", false, "!!!表示過多注意!!!non-stop ファイル内検索時にまとめて表示")
-	forS := flag.String("for", "", "ファイル名検索。正規表現で検索します。\n\tスペースは必ず\\sを指定してください。スペース区切りはAND")
-	fornS := flag.String("forn", "", "forの否定検索版")
-	inS := flag.String("in", "", "!!!表示・処理過多注意!!!\n\tファイル内検索。正規表現で検索します。\n\tスペースは必ず\\sを指定してください。スペース区切りはAND\n\t-forを指定することで読み込むファイルを減らそう！")
-	innS := flag.String("inn", "", "-inの否定検索版")
+	nF := flag.Bool("n", false, "!!!表示過多注意!!!\n\tnon-stop ファイル内検索時にまとめて表示")
+	direS := flag.String("dire", "", "ディレクトリ名検索。パスに対して検索をかけます。-dか-rで検索する必要があります。")
+	direnS := flag.String("diren", "", "-direの否定検索版")
+	fileS := flag.String("file", "", "ファイル名検索。正規表現で検索します。\n\tスペースは必ず\\sを指定してください。スペース区切りはAND")
+	filenS := flag.String("filen", "", "fileの否定検索版")
+	lineS := flag.String("line", "", "!!!表示・処理過多注意!!!\n\tファイル内検索。正規表現で検索します。\n\tスペースは必ず\\sを指定してください。スペース区切りはAND\n\t-fileを指定することで読み込むファイルを減らそう！")
+	linenS := flag.String("linen", "", "-inの否定検索版")
 	fromS := flag.String("from", ".", "検索を開始するディレクトリ")
 	
 	flag.Parse()
@@ -38,33 +42,41 @@ func main(){
 	rFlag = *rF
 	dFlag = *dF
 	nFlag = *nF
-	namesub = strings.Split(*forS, " ")
-	textsub = strings.Split(*inS, " ")
-	namesubnot = strings.Split(*fornS, " ")
-	textsubnot = strings.Split(*innS, " ")
+	diresub = strings.Split(*direS, " ")
+	diresubnot = strings.Split(*direnS, " ")
+	namesub = strings.Split(*fileS, " ")
+	namesubnot = strings.Split(*filenS, " ")
+	linesub = strings.Split(*lineS, " ")
+	linesubnot = strings.Split(*linenS, " ")
 	
-	inF = (*inS != "")
-	innF = (*innS != "")
+	lineF = (*lineS != "") || (*linenS != "")
 	
 	
 	//フラグエラー
-	if dFlag && (inF || innF || nFlag){
-		fmt.Println("<-d(ディレクトリ指定)を使用した場合は、-in/-inn(ファイル内検索)及び-n(ファイル内詳細)はすべて無効です>")
-		inF = false
-		innF = false
+	if dFlag && (lineF || nFlag){
+		fmt.Println("<-d(ディレクトリモード)を使用した場合は、-file/-filen(ファイル名検索)-line/-linen(ファイル内検索)及び-n(ファイル内容連続表示)はすべて無効です>")
+		lineF = false
 		nFlag = false
 	}
-	if !inF && !innF && nFlag{
-		fmt.Println("<-inもしくは-innを指定していないので-nは無効です>\n")
+	if !lineF && nFlag{
+		fmt.Println("<-line/-linenを指定していないので-nは無効です>\n")
+	}
+	if (!rFlag && !dFlag) && *direS != ""{
+		fmt.Println("<-r(再起モード)もしくは-d(ディレクトリモード)を指定していないので-direは無効です。>\n")
 	}
 	//ファイル内検索指定ありで拡張子許可
-	if inF || innF{
+	if lineF{
 		namesub = append(namesub, "(\\.txt$|\\.java$|\\.go$|\\.c$|\\.cpp$|\\.bat$|\\.html$|\\.css$|\\.js$)")
 	}
 	
-	//ファイル探索開始
-	recu(*fromS, "")
+	//ファイル探索開始(最後に\がなかったら付ける)
+	cur := *fromS
+	if cur[len(cur) - 1:] != "\\"{
+		cur = cur + "\\"
+	}
+	recu(cur, "")
 	
+	//個数表示
 	var fileType string
 	if dFlag{
 		fileType = "Directorys"
@@ -77,29 +89,31 @@ func main(){
 
 //カレントディレクトリ、ファイルパス
 func recu(cur string, path string){
-	fds, _ := ioutil.ReadDir(cur + "\\" + path)
+	fds, _ := ioutil.ReadDir(cur + path)
 	for _, v := range fds{
 		if v.IsDir(){
 			if dFlag{
-				if my.MatchAll(v.Name(), namesub) && my.NotMatchAll(v.Name(), namesubnot){
-					fmt.Println(path + v.Name())
+				if my.MatchAll(v.Name(), diresub) && my.NotMatchAll(v.Name(), diresubnot){
+					fmt.Println(cur + path + v.Name())
 					count++
 				}
 			}
 			if rFlag{
 				p := path + v.Name()
-				if p != ""{
-					p = p + "\\"
+				if my.MatchAll(p, diresub) && my.NotMatchAll(p, diresubnot){
+					if p != ""{
+						p = p + "\\"
+					}
+					recu(cur, p)
 				}
-				recu(cur, p)
 			}
 		}else{
 			if !dFlag{
 				if my.MatchAll(v.Name(), namesub) && my.NotMatchAll(v.Name(), namesubnot){
-					if inF || innF{
-						fileCheck(cur + "\\" +  path + v.Name())
+					if lineF{
+						fileCheck(cur + path + v.Name())
 					}else{
-						fmt.Println(path + v.Name())
+						fmt.Println(cur + path + v.Name())
 						count++
 					}
 				}
@@ -113,7 +127,7 @@ func fileCheck(name string){
 	arr := strings.Split(str, "\n")
 	str = ""
 	for n, v := range arr{
-		if my.MatchAll(v, textsub) && my.NotMatchAll(v, textsubnot){
+		if my.MatchAll(v, linesub) && my.NotMatchAll(v, linesubnot){
 			line := strings.Replace(v, "\t", " ", -1)
 			num := strconv.Itoa(n + 1)
 			str =  str + num + "\t" + line + "\n"
