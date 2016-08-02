1cda4dc3722619264825da2320bd6115def94bb4
intelfike <intelfike@gmail.com>
2016-08-02 00:07:14 -0700

    右クリックを拡張するやつ追加

---
+++ goworks/main/gomyshell/rightClickExtend.go
@@ -0,0 +1,27 @@
+package main
+
+import(
+	"./my"
+	"fmt"
+	"strings"
+)
+
+func main(){
+	cmd := []string{"reg", "query", "HKEY_CLASSES_ROOT\\Directory\\Background\\shell\\cmd", "/v", "Extended"}
+	str, _ := my.AutoEnc(my.Execute(cmd))
+	
+	if !strings.Contains(str, "エラー"){
+		fmt.Println("右クリックを拡張します。")
+		cmd = []string{"reg", "delete", "HKEY_CLASSES_ROOT\\Directory\\Background\\shell\\cmd", "/v", "Extended", "/f"}
+	}else{
+		fmt.Println("右クリックの拡張を終了します。")
+		cmd = []string{"reg", "add", "HKEY_CLASSES_ROOT\\Directory\\Background\\shell\\cmd", "/v", "Extended", "/f"}
+	}
+	
+	str, _ = my.AutoEnc(my.Execute(cmd))
+	fmt.Println(str)
+	
+	fmt.Print("Enterで終了:")
+	var s string
+	fmt.Scanln(&s)
+}
