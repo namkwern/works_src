130b984869626db20382db121da308c8819da007
intelfike <intelfike@gmail.com>
2016-09-07 02:23:35 +0900

    改良　同値対策＆動作確認済み

--- goworks/main/sort/sorts/quick.go
+++ goworks/main/sort/sorts/quick.go
@@ -1,8 +1,6 @@
 //命名ルール 先頭=q|Q
 package sorts
-
-import(
-)
+import()
 
 //int(32or64)型スライスに対してソート
 func Qsort(data []int){
@@ -10,7 +8,6 @@ func Qsort(data []int){
 	if len(L) > 1{
 		Qsort(L)
 	}
-	
 	if len(R) > 1{
 		Qsort(R)
 	}
@@ -18,25 +15,31 @@ func Qsort(data []int){
 
 //基準(最初と最後の要素の平均)をもとにスライス左右に振り分ける、クイックソートの最小単位の処理
 func qset(data []int) ([]int, []int){
-	start := 0
-	end := len(data) - 1
+	start, end := 0, len(data) - 1
 	center := (data[start] + data[end]) >> 1
+	f := true
 	outer:for{
 		for data[start] < center{
-			if start == end{
-				break outer
-			}
+			if start >= end{break outer}
 			start++
 		}
-		
 		for data[end] > center{
-			if start == end{
-				break outer
-			}
+			if start >= end{break outer}
 			end--
 		}
 		
-		data[start],data[end] = data[end],data[start]
+		if data[start] == data[end]{//同値要素回避処理
+			if f{
+				start++
+			}else{
+				end--
+			}
+			f = !f
+			if start >= end{break outer}
+		}else{//交換処理
+			data[start],data[end] = data[end],data[start]
+			f = true
+		}
 	}
 	return data[:start], data[start:]
 }
