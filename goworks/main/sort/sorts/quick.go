ec6538eb6f18a6ddfa90d13017621355d5eda9a3
intelfike <intelfike@gmail.com>
2016-09-06 14:28:43 +0900

    多数の同値の要素に弱いという弱点を克服

--- goworks/main/sort/sorts/quick.go
+++ goworks/main/sort/sorts/quick.go
@@ -6,23 +6,23 @@ import(
 
 //int(32or64)型スライスに対してソート
 func Qsort(data []int){
-	start := qset(data[:])
-	if len(data[:start]) > 1{
-		Qsort(data[:start])
+	L, R := qset(data[:])
+	if len(L) > 1{
+		Qsort(L)
 	}
 	
-	if len(data[start:]) > 1{
-		Qsort(data[start:])
+	if len(R) > 1{
+		Qsort(R)
 	}
 }
 
 //基準(最初と最後の要素の平均)をもとにスライス左右に振り分ける、クイックソートの最小単位の処理
-func qset(data []int) int{
+func qset(data []int) ([]int, []int){
 	start := 0
 	end := len(data) - 1
 	center := (data[start] + data[end]) >> 1
 	outer:for{
-		for data[start] <= center{
+		for data[start] < center{
 			if start == end{
 				break outer
 			}
@@ -38,5 +38,5 @@ func qset(data []int) int{
 		
 		data[start],data[end] = data[end],data[start]
 	}
-	return start
+	return data[:start], data[start:]
 }
