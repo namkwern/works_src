49d9e007319bfd33bac68d7cf0694ee1639bfc97
intelfike <intelfike@gmail.com>
2016-09-06 14:29:24 +0900

    エラーチェック改善

--- goworks/main/sort/sort.go
+++ goworks/main/sort/sort.go
@@ -8,37 +8,48 @@ import(
 )
 
 func main(){
-	data := ranArr1()
+	data := makeArr2(200000)
 	
-	fmt.Println(data[:10])
+	fmt.Println(data[:10], data[len(data) - 10:])
 	
 	s := time.Now()
-	sorts.Makeheap(data[:])//ソートの実行
+	sorts.Bitsort(data)//ソートの実行
 	e := time.Now()
 	
-	for n := 1; n < len(data); n++{
-		if data[n - 1] > data[n]{
-			fmt.Println("Error")
+	for n, v := range data{
+		if n != 0 && data[n - 1] > v{
+			fmt.Println("Error!!")
 			break
 		}
 	}
 	
 	fmt.Println(e.Sub(s))//時間の表示
-	fmt.Println(data[:10])
+	fmt.Println(data[:100])
 }
 
-func ranArr1() []int{
+func ranArr1(size int) []int{
 	fmt.Print("乱数のシード値を入力してください:")
 	var i int64
 	fmt.Scanf("%d", &i)//乱数のシード値入力
 	rand.Seed(i)
-	return rand.Perm(200000)//ランダムスライス生成
+	return rand.Perm(size)//ランダムスライス生成
 }
 
-func ranArr2() []int{
-	var data [10000]int
+func makeArr1(size int) []int{
+	data := make([]int, size)
 	for n, _ := range data{
-		data[n] = 1 << 62 - 1
+		data[n] = n
+	}
+	return data[:]
+}
+
+func makeArr2(size int) []int{
+	data := make([]int, size)
+	for n, _ := range data{
+		data[n] = n
+		if n > len(data) / 2{
+			data[n] = len(data) - n
+		}
 	}
 	return data[:]
 }
